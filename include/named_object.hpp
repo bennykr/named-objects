312c05f087b39afb3de693894b04fffcbb670d65
Benny Kramer <kramerbenny@gmail.com>
2020-08-12 15:43:06 +0300

    Replaced tabs with spaces.

--- include/named_object.hpp
+++ include/named_object.hpp
@@ -21,101 +21,101 @@ class NamedObject{
 
 private:
 
-	// `inline` means that there exists only one (static) copy of this std::map,
-	// independently of the number of its occurrences in different translation units.
+    // `inline` means that there exists only one (static) copy of this std::map,
+    // independently of the number of its occurrences in different translation units.
     // This obviates the need in a .cpp file.
-	// Note: This currently doesn't work with Intel's compiler (icc 19.0.1).
-	// Can be fixed by adding a .cpp file
-	static inline std::map<std::string, NamedObject*> named_objects;
+    // Note: This currently doesn't work with Intel's compiler (icc 19.0.1).
+    // Can be fixed by adding a .cpp file
+    static inline std::map<std::string, NamedObject*> named_objects;
 
-	std::string name;
+    std::string name;
 
 protected:
 
-	explicit NamedObject(std::string const & name_) : name(name_){
-		attach_name();
-	}
+    explicit NamedObject(std::string const & name_) : name(name_){
+        attach_name();
+    }
 
-	NamedObject(NamedObject const & other) = delete;
-	void operator=(NamedObject const & other) = delete;
+    NamedObject(NamedObject const & other) = delete;
+    void operator=(NamedObject const & other) = delete;
 
-	NamedObject(NamedObject && other) : name() {
-		switch_object(other);
-	}
+    NamedObject(NamedObject && other) : name() {
+        switch_object(other);
+    }
     void operator=(NamedObject && other){
-		detach_name(); // clear current data
-		switch_object(other);
+        detach_name(); // clear current data
+        switch_object(other);
     }
 
 private:
 
-	void attach_name(){
-		assert_with_message(!name.empty(), "NamedObject: name must not be empty");
+    void attach_name(){
+        assert_with_message(!name.empty(), "NamedObject: name must not be empty");
 
-		auto const [iter, success] = named_objects.try_emplace(name, this);
-		(void) iter; // avoid unused variable warning
-    	assert_with_message(success, "NamedObject: name already used");
-	}
+        auto const [iter, success] = named_objects.try_emplace(name, this);
+        (void) iter; // avoid unused variable warning
+        assert_with_message(success, "NamedObject: name already used");
+    }
 
-	void switch_object(NamedObject & other){
-		assert_with_message(!other.name.empty(),
-							"NamedObject: moved object already detached");
+    void switch_object(NamedObject & other){
+        assert_with_message(!other.name.empty(),
+                            "NamedObject: moved object already detached");
 
-		auto iter = named_objects.find(other.name);
-    	assert_with_message(named_objects.end() != iter,
-							"NamedObject: moved object missing from map");
+        auto iter = named_objects.find(other.name);
+        assert_with_message(named_objects.end() != iter,
+                            "NamedObject: moved object missing from map");
 
-		name = other.name;
-	    iter->second = this;
-		other.name.clear();
-	}
+        name = other.name;
+        iter->second = this;
+        other.name.clear();
+    }
 
-	void detach_name(){
-		if (!name.empty()){
-			size_t const num_erased = named_objects.erase(name);
-			assert_with_message(num_erased > 0,
-								"NamedObject: trying to detache an object missing from map");
-			name.clear();
-		}
-	}
+    void detach_name(){
+        if (!name.empty()){
+            size_t const num_erased = named_objects.erase(name);
+            assert_with_message(num_erased > 0,
+                                "NamedObject: trying to detache an object missing from map");
+            name.clear();
+        }
+    }
 
 public:
 
-	virtual ~NamedObject(){
+    virtual ~NamedObject(){
         detach_name();
     }
-	
-	static NamedObject * find(std::string const & name_){
-		auto const iter = named_objects.find(name_);
-		if (named_objects.end() == iter){
-			return nullptr;
-		}
-		return iter->second;
-	}
+    
+    static NamedObject * find(std::string const & name_){
+        auto const iter = named_objects.find(name_);
+        if (named_objects.end() == iter){
+            return nullptr;
+        }
+        return iter->second;
+    }
 
-	static NamedObject & get(std::string const & name_){
-		auto ptr = find(name_);
-		assert_with_message(ptr != nullptr, "NamedObject: name missing from map");
-		return *ptr;
-	}
+    static NamedObject & get(std::string const & name_){
+        auto ptr = find(name_);
+        assert_with_message(ptr != nullptr, "NamedObject: name missing from map");
+        return *ptr;
+    }
 
-	void reset_name(std::string const & new_name){
-		detach_name();
-		name = new_name;
-		attach_name();
-	}
+    void reset_name(std::string const & new_name){
+        detach_name();
+        name = new_name;
+        attach_name();
+    }
 
-	std::string const & get_name(){
-		return name;
-	}
+    std::string const & get_name(){
+        return name;
+    }
 
     // Extensions to header here.
-	
+    
     // For example:
     // virtual void write_hdf5(H5::Group & h5group, std::string const & name_) const = 0;
-	// void write_hdf5(H5::Group & h5group) const {
-	// 	write_hdf5(h5group, name);
-	// }
+    // void write_hdf5(H5::Group & h5group) const {
+    //     write_hdf5(h5group, name);
+    // }
 };
 
 #endif /* NAMED_OBJECT_BASE_HPP_ */
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,5 @@
 #include "gtest/gtest.h"
-
 #include "named_object.hpp"
-
-#include <iostream>
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
@@ -18,22 +15,22 @@ struct AttachedObject : public NamedObject {
 };
 
 TEST(NamedObject, RetrieveNullPointer) {
-	ASSERT_EQ(NamedObject::find(AttachedObject::default_name), nullptr);
+    ASSERT_EQ(NamedObject::find(AttachedObject::default_name), nullptr);
 }
 
 TEST(NamedObject, RetrievePointer) {
     AttachedObject attached_object;
-	ASSERT_EQ(NamedObject::find(AttachedObject::default_name), &attached_object);
+    ASSERT_EQ(NamedObject::find(AttachedObject::default_name), &attached_object);
 }
 
 TEST(NamedObject, GetObjectReference) {
     AttachedObject attached_object;
-	ASSERT_EQ(&NamedObject::get(AttachedObject::default_name), &attached_object);
+    ASSERT_EQ(&NamedObject::get(AttachedObject::default_name), &attached_object);
 }
 
 TEST(NamedObject, GetName) {
     AttachedObject attached_object;
-	ASSERT_EQ(attached_object.get_name(), AttachedObject::default_name);
+    ASSERT_EQ(attached_object.get_name(), AttachedObject::default_name);
 }
 
 TEST(NamedObject, ResetName) {
@@ -43,13 +40,13 @@ TEST(NamedObject, ResetName) {
     attached_object.reset_name(new_name);
 
     ASSERT_DEATH(NamedObject::get(AttachedObject::default_name), "name missing");
-	ASSERT_EQ(&NamedObject::get(new_name), &attached_object);
+    ASSERT_EQ(&NamedObject::get(new_name), &attached_object);
 }
 
 TEST(NamedObject, DeleteObject) {
     {   
         AttachedObject attached_object;
-	    ASSERT_EQ(NamedObject::find(AttachedObject::default_name), &attached_object);
+        ASSERT_EQ(NamedObject::find(AttachedObject::default_name), &attached_object);
     }
     ASSERT_EQ(NamedObject::find(AttachedObject::default_name), nullptr);
 }
@@ -57,18 +54,18 @@ TEST(NamedObject, DeleteObject) {
 TEST(NamedObject, MoveAssignment) {
     AttachedObject object_1("1"), object_2("2");
 
-	ASSERT_EQ(NamedObject::find("1"), &object_1);
-   	ASSERT_EQ(NamedObject::find("2"), &object_2);
+    ASSERT_EQ(NamedObject::find("1"), &object_1);
+    ASSERT_EQ(NamedObject::find("2"), &object_2);
 
     object_1 = std::move(object_2);
-	ASSERT_EQ(NamedObject::find("1"), nullptr);
-   	ASSERT_EQ(NamedObject::find("2"), &object_1);
+    ASSERT_EQ(NamedObject::find("1"), nullptr);
+    ASSERT_EQ(NamedObject::find("2"), &object_1);
 }
 
 TEST(NamedObject, MoveConstructor) {
     AttachedObject object_1("1");
-	ASSERT_EQ(NamedObject::find("1"), &object_1);
+    ASSERT_EQ(NamedObject::find("1"), &object_1);
 
     AttachedObject object_2(std::move(object_1));
-	ASSERT_EQ(NamedObject::find("1"), &object_2);
+    ASSERT_EQ(NamedObject::find("1"), &object_2);
 }
